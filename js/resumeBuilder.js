12f02101bf4b373ab14c4c2ca202c90bd320d616
Jason Crouse <jc8786@att.com>
2015-08-24 13:19:44 -0400

    Added Name and Role to the resume using jquery methods - replace, prepend

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,2 +1,18 @@
-$("#main").append("Jason Crouse");
-var awesomeThoughts = "I am Jason Crouse and I am AWESOME!";
+//$("#main").append("Jason Crouse");
+//var awesomeThoughts = "I am Jason Crouse and I am AWESOME!";
+
+//Declare Variables
+var name = "Jason Crouse";
+var role = "Senior Technical Team Lead";
+var formattedName;
+var formattedRole;
+
+//Use jquery replace method to replace HTMLheaderName/HTLMheaderRole with my name and role
+formattedName = HTMLheaderName.replace("%data%", name);						
+formattedRole = HTMLheaderRole.replace("%data%", role);
+
+//Use jquery append/prepend to add to the header section of the HTML Page
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
+
+
